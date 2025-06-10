dea9f45bcdf67c0b498368abd7d1c92364d4c8eb
ranyoung <fks1311@gmail.com>
2025-06-10 17:37:16 +0900

    chore: webpack 설정 마무리

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// 원래는 root로 되어있음. public의 index.html의 root로 되어 있는데
+// 웹팩 설정에서 src의 index.html부터 번들링을 시작한다했으니 해당 html의 id를 가져와야함.
+const root = ReactDOM.createRoot(document.getElementById("content") as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
