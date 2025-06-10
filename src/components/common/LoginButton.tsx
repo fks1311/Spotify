64c2a9f4c6861fac0ff289d846ab144e26b1fff4
ranyoung <fks1311@gmail.com>
2025-06-11 06:59:31 +0900

    feat: Side-bar 생성

---
+++ src/components/common/LoginButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+const LoginButton = () => {
+  return (
+    <Button variant="contained" color="secondary" size="large">
+      Log in
+    </Button>
+  );
+};
+
+export default LoginButton;
---
+++ src/components/side-bar/LibraryHead.tsx
@@ -0,0 +1,33 @@
+import { Box, styled, Typography, Button } from "@mui/material";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import AddIcon from "@mui/icons-material/Add";
+
+const LibraryHead = () => {
+  const handleCreatePlaylist = () => {
+    // 나중에 추가 예정
+  };
+
+  return (
+    <Head>
+      <Box display="flex">
+        <BookmarkIcon sx={{ marginRight: "20px" }} />
+        <Typography variant="h2" fontWeight={700}>
+          Your Library
+        </Typography>
+      </Box>
+      <Button onClick={handleCreatePlaylist}>
+        <AddIcon />
+      </Button>
+    </Head>
+  );
+};
+
+const Head = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  padding: "8px",
+
+  justifyContent: "space-between",
+});
+
+export default LibraryHead;
