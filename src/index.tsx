6ffe0fb3b785f56ef61ba88e4f34c24ad17e8bc3
ranyoung <fks1311@gmail.com>
2025-06-10 18:51:09 +0900

    feat: 라우터 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router";
 
 // 원래는 root로 되어있음. public의 index.html의 root로 되어 있는데
 // 웹팩 설정에서 src의 index.html부터 번들링을 시작한다했으니 해당 html의 id를 가져와야함.
 const root = ReactDOM.createRoot(document.getElementById("content") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/SearchPage/SearchWithPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SearchWithPage() {
+  return <div>SearchWithPage</div>;
+}
