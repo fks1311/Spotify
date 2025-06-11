77a4317c3dc5a15b449686636a618a1808c81412
ranyoung <fks1311@gmail.com>
2025-06-12 08:28:50 +0900

    refactor: axios 유틸화로 변경

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -1,15 +1,10 @@
-import axios from "axios";
-import { SPOTIFY_BASE_URL } from "../configs/commonConfig";
+import { api } from "../utils/api";
 import { User } from "../models/user";
 
 /** Spotify API : 프로필 정보를 가져옵니다. */
 export const getCurrentUserProfile = async (): Promise<User> => {
   try {
-    const response = await axios.get(`${SPOTIFY_BASE_URL}/me`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-      },
-    });
+    const response = await api.get(`/me`);
     return response.data;
   } catch (error) {
     throw new Error("fail to fetch user profile");
