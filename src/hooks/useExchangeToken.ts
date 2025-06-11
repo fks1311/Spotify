aef84f8b64883cab04a4ada6158d131da4ccf61f
ranyoung <fks1311@gmail.com>
2025-06-11 19:45:43 +0900

    chore: 주석 수정

--- src/hooks/useExchangeToken.ts
+++ src/hooks/useExchangeToken.ts
@@ -2,7 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import { exchageToken } from "../apis/authApi";
 import { ExchageTokenResponse } from "../models/auth";
 
-/** 사용자가 권한 부여 요청을 수락하면 권한 부여 코드를 액세스 토큰으로 교환합니다. */
+/** 사용자가 권한 부여 요청을 수락하면 권한 부여 코드를 액세스 토큰으로 교환하는 커스텀 훅입니다. */
 export const useExchangeToken = () => {
   return useMutation<ExchageTokenResponse, Error, { code: string; codeVerifier: string }>({
     mutationFn: ({ code, codeVerifier }) => {
