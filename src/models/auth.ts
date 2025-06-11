b68b8e8de6f41e2dc11b39a8939be6b2ba1471d0
ranyoung <fks1311@gmail.com>
2025-06-11 17:24:23 +0900

    feat: PKCE params 타입 생성

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -3,3 +3,13 @@ export interface IClientCredentialTokenResponse {
   token_type: string;
   expires_in: number;
 }
+
+// spotify PKCE의 params
+export interface AuthUrlParams {
+  response_type: "code";
+  client_id: string;
+  scope: string;
+  code_challenge_method: "S256";
+  code_challenge: string;
+  redirect_uri: string;
+}
