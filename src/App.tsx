771795894dce61047b716c454f6ea04355c0247b
ranyoung <fks1311@gmail.com>
2025-06-11 19:01:40 +0900

    feat: 로그인 액션 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router";
+import { useExchangeToken } from "./hooks/useExchangeToken";
 const AppLayout = React.lazy(() => import("./layout/AppLayout"));
 const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
 const SearchPage = React.lazy(() => import("./pages/SearchPage/SearchPage"));
@@ -8,6 +9,18 @@ const PlaylistDetailPage = React.lazy(() => import("./pages/PlaylistPage/Playlis
 const PlaylistPage = React.lazy(() => import("./pages/PlaylistPage/PlaylistPage"));
 
 function App() {
+  const urlParams = new URLSearchParams(window.location.search);
+  let code = urlParams.get("code");
+  const codeVerifier = localStorage.getItem("code_verifier");
+
+  const { mutate: exchageToken } = useExchangeToken();
+
+  useEffect(() => {
+    if (code && codeVerifier) {
+      exchageToken({ code, codeVerifier });
+    }
+  }, [code, codeVerifier]);
+
   return (
     <Suspense fallback={<div>loading...</div>}>
       <Routes>
