2f1da8b54f4d4cb6569d17701c24d0ddc85ef702
ranyoung <fks1311@gmail.com>
2025-06-11 18:59:30 +0900

    feat: 권한 부여 코드 -> 액세스 토큰으로 교환 함수 생성

---
+++ src/hooks/useExchangeToken.ts
@@ -0,0 +1,15 @@
+import { useMutation } from "@tanstack/react-query";
+import { exchageToken } from "../apis/authApi";
+import { ExchageTokenResponse } from "../models/auth";
+
+/** 사용자가 권한 부여 요청을 수락하면 권한 부여 코드를 액세스 토큰으로 교환합니다. */
+export const useExchangeToken = () => {
+  return useMutation<ExchageTokenResponse, Error, { code: string; codeVerifier: string }>({
+    mutationFn: ({ code, codeVerifier }) => {
+      return exchageToken(code, codeVerifier);
+    },
+    onSuccess: (data) => {
+      localStorage.setItem("access_token", data.access_token);
+    },
+  });
+};
