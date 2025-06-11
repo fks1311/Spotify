cf9848b872a2b9cce2646a1445cdd507e275f25c
ranyoung <fks1311@gmail.com>
2025-06-11 17:55:49 +0900

    chore: Spotify Auth 관련 API 주석 처리

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,20 +1,20 @@
 /*spotify에서 암호화 관련된 함수들 */
 
-// Code Verifier : 코드 검증기
+/** Spotify 암호화와 관련된 함수로 Code Verifier(코드 검증)합니다. */
 export const generateRandomString = (length: number): string => {
   const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const values = crypto.getRandomValues(new Uint8Array(length));
   return values.reduce((acc, x) => acc + possible[x % possible.length], "");
 };
 
-// 해시 값 생성
+/** Spotify 암호화와 관련된 함수로 generateRandomString 함수의 결과로 해시 값을 생성합니다. */
 export const sha256 = async (plain: string): Promise<ArrayBuffer> => {
   const encoder = new TextEncoder();
   const data = encoder.encode(plain);
   return window.crypto.subtle.digest("SHA-256", data); // 이거 타입이 ArrayBuffer
 };
 
-// base64 encode : 위의 sha256의 결과를 base64로 반환하는 함수
+/** Spotify 암호화와 관련된 함수로 sha256 함수의 결과를 base64로 인코딩합니다. */
 export const base64encode = (input: ArrayBuffer): string => {
   return btoa(String.fromCharCode(...new Uint8Array(input)))
     .replace(/=/g, "")
