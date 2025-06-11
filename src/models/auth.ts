40f9d5bf47aede2dacfec290cfa754e6ccebd7b3
ranyoung <fks1311@gmail.com>
2025-06-11 19:00:54 +0900

    feat: 권한 부여 액세스 토큰 교환 API 응답값 interface 생성

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -5,7 +5,7 @@ export interface IClientCredentialTokenResponse {
   expires_in: number;
 }
 
-/** spotify PKCE의 params */
+/** spotify 사용자 권한 요청 params */
 export interface AuthUrlParams {
   response_type: "code";
   client_id: string;
@@ -13,4 +13,13 @@ export interface AuthUrlParams {
   code_challenge_method: "S256";
   code_challenge: string;
   redirect_uri: string;
+}
+
+/** Spotify 권한 부여 액세스 토큰 교환 API 응답값 */
+export interface ExchageTokenResponse {
+  access_token: string;
+  token_type: string;
+  scope: string;
+  expires_in: number;
+  refresh_token: string;
 }
