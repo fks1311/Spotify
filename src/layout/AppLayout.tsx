5a46bd1b376afd9c5f0af585365250a179c2f7af
ranyoung <fks1311@gmail.com>
2025-06-11 12:35:08 +0900

    refactor: side-bar layout 폴더 구조 변경

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,36 +1,12 @@
-import { Box, styled, Typography } from "@mui/material";
-import { NavLink, Outlet } from "react-router";
-import HomeIcon from "@mui/icons-material/Home";
-import SearchIcon from "@mui/icons-material/Search";
-import LibraryHead from "../components/side-bar/LibraryHead";
-import Library from "./Library";
-import Navbar from "../components/side-bar/Navbar";
+import { Box, styled } from "@mui/material";
+import { Outlet } from "react-router";
+import Navbar from "./Navbar";
+import Side_bar from "./side-bar/Side_bar";
 
 const AppLayout = () => {
   return (
     <Layout>
-      <Sidebar>
-        <ContentBox>
-          <NavList>
-            <StyledNavLink to={"/"}>
-              <HomeIcon />
-              <Typography variant="h2" fontWeight={700}>
-                Home
-              </Typography>
-            </StyledNavLink>
-            <StyledNavLink to={"/"}>
-              <SearchIcon />
-              <Typography variant="h2" fontWeight={700}>
-                Search
-              </Typography>
-            </StyledNavLink>
-          </NavList>
-        </ContentBox>
-        <ContentBox height={"100%"}>
-          <LibraryHead />
-          <Library />
-        </ContentBox>
-      </Sidebar>
+      <Side_bar />
       <ContentBox>
         <Navbar />
         <Outlet />
@@ -45,16 +21,6 @@ const Layout = styled("div")({
   padding: "8px",
 });
 
-const Sidebar = styled("div")(({ theme }) => ({
-  width: "331px",
-  height: "100%",
-  display: "flex",
-  flexDirection: "column",
-  [theme.breakpoints.down("sm")]: {
-    display: "none",
-  },
-}));
-
 const ContentBox = styled(Box)(({ theme }) => ({
   borderRadius: "8px",
   backgroundColor: theme.palette.background.paper,
@@ -63,22 +29,6 @@ const ContentBox = styled(Box)(({ theme }) => ({
   padding: "8px",
   marginBottom: "8px",
   marginRight: "8px",
-}));
-
-const NavList = styled("ul")({
-  listStyle: "none",
-  padding: 0,
-  margin: 0,
-});
-
-const StyledNavLink = styled(NavLink)(({ theme }) => ({
-  textDecoration: "none",
-  display: "flex",
-  alignItems: "center",
-  gap: "20px",
-  color: theme.palette.text.secondary,
-  "&:hover": { color: theme.palette.text.primary },
-  ".active": { color: theme.palette.text.primary },
 }));
 
 export default AppLayout;
