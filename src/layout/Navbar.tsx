559dccfe996f56b4f675e4bbbf33225ff3855c67
ranyoung <fks1311@gmail.com>
2025-06-11 19:49:35 +0900

    feat: 로그인 상태에 따른 버튼 변경

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,12 +1,39 @@
-import { Box } from "@mui/material";
+import { Box, styled } from "@mui/material";
 import LoginButton from "../components/LoginButton";
+import { useGetCurrentUserProfile } from "../hooks/useGetCurrentUserProfile";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const Navbar = () => {
+  const { data: userProfile } = useGetCurrentUserProfile();
+
+  const IsProfileImg = () => {
+    return (
+      <ProfileImg>
+        <>
+          {userProfile?.images && userProfile?.images?.length > 0 ? (
+            <img src={userProfile.images[0].url} />
+          ) : (
+            <AccountCircleIcon fontSize="large" />
+          )}
+        </>
+        <div>{userProfile?.display_name}</div>
+      </ProfileImg>
+    );
+  };
+
   return (
     <Box display={"flex"} justifyContent={"flex-end"} alignItems={"center"} height={"64px"}>
-      <LoginButton />
+      {userProfile ? <IsProfileImg /> : <LoginButton />}
     </Box>
   );
 };
+
+const ProfileImg = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "0.5rem",
+  padding: "8px",
+});
 
 export default Navbar;
