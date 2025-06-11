05dab6de1b66fb69dd056c7b1779ff51cc3a26d4
ranyoung <fks1311@gmail.com>
2025-06-11 11:28:56 +0900

    feat: react-query 설치로 인한 QueryClientProvider 설정

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,20 @@ import App from "./App";
 import { BrowserRouter } from "react-router";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // 원래는 root로 되어있음. public의 index.html의 root로 되어 있는데
 // 웹팩 설정에서 src의 index.html부터 번들링을 시작한다했으니 해당 html의 id를 가져와야함.
 const root = ReactDOM.createRoot(document.getElementById("content") as HTMLElement);
+const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
