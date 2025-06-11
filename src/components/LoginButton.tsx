60f507c3cfed731b5ee742d9817acfc1ad0e8412
ranyoung <fks1311@gmail.com>
2025-06-11 17:27:37 +0900

    feat: 로그인 함수 생성

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@mui/material";
+import { getSpotifyAuthUrl } from "../utils/getSpotifyAuthUrl";
 
 const LoginButton = () => {
+  const login = () => getSpotifyAuthUrl();
+
   return (
-    <Button variant="contained" color="secondary" size="large">
+    <Button variant="contained" color="secondary" size="large" onClick={login}>
       Log in
     </Button>
   );
