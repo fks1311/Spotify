8b15a632a9ff51b0dc6be5b079ed2409c9159e5b
ranyoung <fks1311@gmail.com>
2025-06-13 16:53:18 +0900

    feat: 로그아웃 기능 생성

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -2,18 +2,32 @@ import { Box, styled } from "@mui/material";
 import LoginButton from "../components/LoginButton";
 import { useGetCurrentUserProfile } from "../hooks/useGetCurrentUserProfile";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { getLocalStorageSafe } from "../utils/localStorage";
+import { useSetRecoilState } from "recoil";
+import { triggerAtom } from "../utils/atom";
 
+// trigger
 const Navbar = () => {
+  const setTrigger = useSetRecoilState(triggerAtom);
   const { data: userProfile } = useGetCurrentUserProfile();
+  const accessToken = getLocalStorageSafe("access_token");
+  const url = new URL(window.location.href);
+
+  const logout = () => {
+    window.localStorage.removeItem("access_token");
+    url.search = "";
+    window.history.replaceState(null, "", url.toString());
+    setTrigger((prev) => prev + 1);
+  };
 
   const IsProfileImg = () => {
     return (
       <ProfileImg>
         <>
           {userProfile?.images && userProfile?.images?.length > 0 ? (
-            <img src={userProfile.images[0].url} />
+            <img src={userProfile.images[0].url} onClick={logout} />
           ) : (
-            <AccountCircleIcon fontSize="large" />
+            <AccountCircleIcon fontSize="large" onClick={logout} />
           )}
         </>
         <div>{userProfile?.display_name}</div>
@@ -23,7 +37,7 @@ const Navbar = () => {
 
   return (
     <Box display={"flex"} justifyContent={"flex-end"} alignItems={"center"} height={"64px"}>
-      {userProfile ? <IsProfileImg /> : <LoginButton />}
+      {!accessToken || !userProfile ? <LoginButton /> : <IsProfileImg />}
     </Box>
   );
 };
@@ -34,6 +48,7 @@ const ProfileImg = styled("div")({
   alignItems: "center",
   gap: "0.5rem",
   padding: "8px",
+  cursor: "pointer",
 });
 
 export default Navbar;
