c9bf013af7c9938782092a5ee57690a21cca819f
ranyoung <fks1311@gmail.com>
2025-06-13 16:49:35 +0900

    refactor: playlist 호출 조건 설정

--- src/hooks/useGetCurrentUserPlaylists.ts
+++ src/hooks/useGetCurrentUserPlaylists.ts
@@ -1,13 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCurrentUserPlaylists } from "../apis/playlistApi";
 import { GetCurrentUserPlaylistRequest } from "../models/playlist";
+import { getLocalStorageSafe } from "../utils/localStorage";
 
 /** 플레이리스트 목록을 가져오는 커스텀 훅입니다. */
 export const useGetCurrentUserPlaylists = ({ limit, offset }: GetCurrentUserPlaylistRequest) => {
+  const accessToken = getLocalStorageSafe("access_token");
+
   return useQuery({
     queryKey: ["current-user-playlists"],
     queryFn: () => {
       return getCurrentUserPlaylists({ limit, offset });
     },
+    enabled: !!accessToken,
   });
 };
