33eefc5e5bc27ead33dc735dd5081553c895d566
ranyoung <fks1311@gmail.com>
2025-06-13 16:54:11 +0900

    내 플레이리스트 layout 수정

--- src/layout/side-bar/Library.tsx
+++ src/layout/side-bar/Library.tsx
@@ -1,11 +1,18 @@
+import { LoadingSpinner } from "../../components/LoadingSpinner";
 import PlaylistItem from "../../components/playlist/PlaylistItem";
 import { useGetCurrentUserPlaylists } from "../../hooks/useGetCurrentUserPlaylists";
+import { getLocalStorageSafe } from "../../utils/localStorage";
 import EmptyPlaylist from "./EmptyPlaylist";
 
 const Library = () => {
-  const { data } = useGetCurrentUserPlaylists({ limit: 10, offset: 0 });
+  const accessToken = getLocalStorageSafe("access_token");
+  const { isLoading, data } = useGetCurrentUserPlaylists({ limit: 10, offset: 0 });
 
-  return <>{!data || data?.total === 0 ? <EmptyPlaylist /> : <PlaylistItem items={data.items} />}</>;
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  return <>{!accessToken || !data || data?.total === 0 ? <EmptyPlaylist /> : <PlaylistItem items={data.items} />}</>;
 };
 
 export default Library;
