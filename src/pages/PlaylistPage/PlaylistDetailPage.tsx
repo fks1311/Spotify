46eaa04addc3a67435fb3ad9a3dfa4a26189ec80
ranyoung <fks1311@gmail.com>
2025-06-13 20:20:31 +0900

    feat: 개별 재생목록 함수 호출 및 페이지 생성

--- src/pages/PlaylistPage/PlaylistDetailPage.tsx
+++ src/pages/PlaylistPage/PlaylistDetailPage.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { Avatar, Box, styled, Typography } from "@mui/material";
+import { useGetPlaylist } from "../../hooks/useGetPlaylist";
+import { Navigate, useParams } from "react-router";
 
 const PlaylistDetailPage = () => {
-  return <div>PlaylistDetailPage</div>;
+  const { id } = useParams<{ id: string }>();
+
+  if (id === undefined) return <Navigate to="/" />;
+  const { data: playlist } = useGetPlaylist({ playlist_id: id });
+
+  return (
+    <Container>
+      <PlaylistHeader>
+        <LargeAvarta src={playlist?.images?.[0]?.url} variant="rounded" />
+        <div>
+          <LargeTypography>{playlist?.name}</LargeTypography>
+          <Typography variant="h1">{playlist?.owner?.display_name}</Typography>
+        </div>
+      </PlaylistHeader>
+    </Container>
+  );
 };
 
+const Container = styled("div")({});
+const PlaylistHeader = styled(Box)({
+  display: "flex",
+  gap: `1rem`,
+  width: "100%",
+  padding: "1rem",
+});
+const LargeAvarta = styled(Avatar)({
+  width: 200,
+  height: 200,
+  borderRadius: 8,
+});
+const LargeTypography = styled(Typography)({
+  fontSize: `3.5rem`,
+});
 export default PlaylistDetailPage;
