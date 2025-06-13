8f7f6e2a3bc6cffa196d9ea3be7e74354d189f60
ranyoung <fks1311@gmail.com>
2025-06-13 17:28:25 +0900

    style: scrollbar 수정

--- src/layout/side-bar/Library.tsx
+++ src/layout/side-bar/Library.tsx
@@ -4,28 +4,37 @@ import PlaylistItem from "../../components/playlist/PlaylistItem";
 import { useGetCurrentUserPlaylists } from "../../hooks/useGetCurrentUserPlaylists";
 import { getLocalStorageSafe } from "../../utils/localStorage";
 import EmptyPlaylist from "./EmptyPlaylist";
+import { useState } from "react";
 
+interface ContainerProps {
+  isMouseOver: boolean;
+}
 const Library = () => {
   const accessToken = getLocalStorageSafe("access_token");
   const { isLoading, data } = useGetCurrentUserPlaylists({ limit: 50, offset: 0 });
+  const [mouseOver, setMouseOver] = useState<boolean>(false);
 
   if (isLoading) {
     return <LoadingSpinner />;
   }
 
   return (
-    <Container>
+    <Container isMouseOver={mouseOver} onMouseOver={() => setMouseOver(true)} onMouseOut={() => setMouseOver(false)}>
       {!accessToken || !data || data?.total === 0 ? <EmptyPlaylist /> : <PlaylistItem items={data.items} />}
     </Container>
   );
 };
 
-const Container = styled("div")({
+const Container = styled("div")<ContainerProps>(({ theme, isMouseOver }) => ({
   height: "100%",
   overflowY: "auto",
   maxHeight: "calc(100vh - 240px)",
   "&::-webkit-scrollbar": {
-    display: "none",
+    width: "4px",
   },
-});
+  "&::-webkit-scrollbar-thumb": {
+    background: isMouseOver ? theme.palette.primary.main : "transparent",
+    borderRadius: "10px",
+  },
+}));
 export default Library;
