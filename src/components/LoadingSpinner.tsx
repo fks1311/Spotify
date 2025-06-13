7f3b0501d71891aff38560d0ae69f92176bb0224
ranyoung <fks1311@gmail.com>
2025-06-13 16:32:50 +0900

    feat: LoadingSpinner 컴포넌트 생성

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,7 +1,16 @@
-import { styled } from "@mui/material";
+import { CircularProgress, styled } from "@mui/material";
 
 export const LoadingSpinner = () => {
-  return <div>LoadingSpinner</div>;
+  return (
+    <Container className="loading">
+      <CircularProgress variant="indeterminate" />
+    </Container>
+  );
 };
 
-const Container = styled("div")({});
+const Container = styled("div")({
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
