713ae44da9ad7bfc27910bad45e5bf45f160fbc8
ranyoung <fks1311@gmail.com>
2025-06-13 17:07:36 +0900

    style: 플레이리스트 scrollbar 수정

--- src/layout/side-bar/Library.tsx
+++ src/layout/side-bar/Library.tsx
@@ -1,3 +1,4 @@
+import { styled } from "@mui/material";
 import { LoadingSpinner } from "../../components/LoadingSpinner";
 import PlaylistItem from "../../components/playlist/PlaylistItem";
 import { useGetCurrentUserPlaylists } from "../../hooks/useGetCurrentUserPlaylists";
@@ -6,13 +7,25 @@ import EmptyPlaylist from "./EmptyPlaylist";
 
 const Library = () => {
   const accessToken = getLocalStorageSafe("access_token");
-  const { isLoading, data } = useGetCurrentUserPlaylists({ limit: 10, offset: 0 });
+  const { isLoading, data } = useGetCurrentUserPlaylists({ limit: 50, offset: 0 });
 
   if (isLoading) {
     return <LoadingSpinner />;
   }
 
-  return <>{!accessToken || !data || data?.total === 0 ? <EmptyPlaylist /> : <PlaylistItem items={data.items} />}</>;
+  return (
+    <Container>
+      {!accessToken || !data || data?.total === 0 ? <EmptyPlaylist /> : <PlaylistItem items={data.items} />}
+    </Container>
+  );
 };
 
+const Container = styled("div")({
+  height: "100%",
+  overflowY: "auto",
+  maxHeight: "calc(100vh - 240px)",
+  "&::-webkit-scrollbar": {
+    display: "none",
+  },
+});
 export default Library;
