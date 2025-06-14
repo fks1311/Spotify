4a73361d4e5622ef3c4a097e7d4f60abc4bd9a69
ranyoung <fks1311@gmail.com>
2025-06-14 12:47:28 +0900

    feat: search API 및 Hook 생성

---
+++ src/hooks/useSearchItemsByKeyword.ts
@@ -0,0 +1,33 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { searchItemsByKeyword } from "../apis/searchApi";
+import { SearchRequestParams } from "../models/search";
+import { useClientCredentialToken } from "./useClientCredentialToken";
+
+export const useSearchItemsByKeyword = (params: SearchRequestParams) => {
+  const clientCredentialToken = useClientCredentialToken();
+
+  return useInfiniteQuery({
+    queryKey: ["search", params],
+    queryFn: ({ pageParam = 0 }) => {
+      if (!clientCredentialToken) throw new Error("No Token available");
+      return searchItemsByKeyword(clientCredentialToken, { ...params, offset: pageParam });
+    },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      const nextPageUrl =
+        lastPage.track?.next ||
+        lastPage.artist?.next ||
+        lastPage.album?.next ||
+        lastPage.playlists?.next ||
+        lastPage.show?.next ||
+        lastPage.episode?.next ||
+        lastPage.audiobook?.next;
+
+      if (nextPageUrl) {
+        const nextOffset = new URL(nextPageUrl).searchParams.get("offset");
+        return nextOffset ? parseInt(nextOffset) : undefined;
+      }
+      return undefined;
+    },
+  });
+};
