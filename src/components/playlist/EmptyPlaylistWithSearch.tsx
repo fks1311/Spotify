9b1d3d8166710c1845218392b04df7544adbd4ac
ranyoung <fks1311@gmail.com>
2025-06-14 16:13:22 +0900

    feat: 검색 결과 컴포넌트 수정

--- src/components/playlist/EmptyPlaylistWithSearch.tsx
+++ src/components/playlist/EmptyPlaylistWithSearch.tsx
@@ -8,21 +8,27 @@ const EmptyPlaylistWithSearch = () => {
   const [keyword, setKeyword] = useState<string>("");
   const { data, error, isLoading } = useSearchItemsByKeyword({
     q: keyword,
-    type: [SEARCH_TYPE.Track],
+    type: Object.values(SEARCH_TYPE),
   });
   const handleSearchKeyword = (e: React.ChangeEvent<HTMLInputElement>) => {
     setKeyword(e.target.value);
   };
 
+  if (error) {
+  }
+
   return (
-    <div>
+    <div style={{ width: "100%" }}>
       <Typography variant="h1" my={"10px"}>
-        Let's find something for your playlist
+        플레이리스트에 추가할 곡을 찾아보세요
       </Typography>
-      <TextField value={keyword} onChange={handleSearchKeyword} />
+      <TextField value={keyword} onChange={handleSearchKeyword} style={{ width: "100%" }} />
       {data?.pages.map((item, idx) => {
-        if (!item.tracks) return false;
-        return <SearchResultList list={item.tracks?.items} />;
+        const validEntries = Object.entries(item).filter(([_, value]) => value?.items?.length > 0);
+        if (validEntries.length === 0) return null;
+        return validEntries.map(([key, value]) => {
+          return <SearchResultList key={`${idx}-${key}`} list={value.items} />;
+        });
       })}
     </div>
   );
--- src/components/playlist/SearchResultList.tsx
+++ src/components/playlist/SearchResultList.tsx
@@ -1,18 +1,53 @@
-import { Typography } from "@mui/material";
+import { Box, Button, styled, TableCell, TableRow, Typography } from "@mui/material";
 import { ITrack } from "../../models/track";
 
 interface SearchResultListProps {
   list: ITrack[];
 }
 const SearchResultList = ({ list }: SearchResultListProps) => {
-  console.log(list);
+  if (!list || list.length === 0) return null;
+
+  const isTrackList = "album" in list[0] && "artists" in list[0];
+
+  if (!isTrackList) return null;
+
   return (
     <div>
-      {list.map((track) => (
-        <Typography variant="h2">{track.name}</Typography>
+      {list.map((track: any) => (
+        <StyledTableRow key={track.id}>
+          <TableCell>
+            <Box display="flex" alignItems="center">
+              <Box>
+                <AlbumImage src={track.album?.images?.[0]?.url} width="40px" />
+              </Box>
+              <Box>
+                <Typography fontWeight={700}>{track.name}</Typography>
+                <Typography color="text.secondary">{track.artists?.[0]?.name ?? "Unknown Artist"}</Typography>
+              </Box>
+            </Box>
+          </TableCell>
+          <TableCell>{track.album?.name}</TableCell>
+          <TableCell>
+            <Button>Add</Button>
+          </TableCell>
+        </StyledTableRow>
       ))}
     </div>
   );
 };
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  width: "100%",
+  "&:hover": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  "& .MuiTableCell-root": {
+    borderBottom: "none",
+  },
+}));
+const AlbumImage = styled("img")({
+  borderRadius: "4px",
+  marginRight: "12px",
+});
 
 export default SearchResultList;
