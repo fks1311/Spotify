3d5e497c0771160aa5004a0645314a409b0ed838
ranyoung <fks1311@gmail.com>
2025-06-16 17:30:31 +0900

    feat: 아이템 추가 후 플레이리스트 아이템 invalid 시켜 refetching

--- src/hooks/useAddItemToPlaylist.ts
+++ src/hooks/useAddItemToPlaylist.ts
@@ -1,9 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AddItemToPlaylistRequest } from "../models/playlist";
 import { addItemToPlaylist } from "../apis/playlistApi";
 import { useGetCurrentUserProfile } from "./useGetCurrentUserProfile";
 
 export const useAddItemToPlaylist = () => {
+  const queryClient = useQueryClient();
   const { data: user } = useGetCurrentUserProfile();
 
   return useMutation({
@@ -15,6 +16,7 @@ export const useAddItemToPlaylist = () => {
     },
     onSuccess: () => {
       console.log(`useAddItemToPlaylist 성공`);
+      queryClient.invalidateQueries({ queryKey: ["playlist-items"] });
     },
   });
 };
