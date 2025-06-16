830bc1e310d85420b0eee1809ed8aee5ccd8f392
ranyoung <fks1311@gmail.com>
2025-06-16 09:54:41 +0900

    feat: 검색결과 없을 경우 조건 처리

--- src/components/playlist/EmptyPlaylistWithSearch.tsx
+++ src/components/playlist/EmptyPlaylistWithSearch.tsx
@@ -1,8 +1,10 @@
-import { TextField, Typography } from "@mui/material";
+import { styled, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import { useSearchItemsByKeyword } from "../../hooks/useSearchItemsByKeyword";
 import { SEARCH_TYPE } from "../../models/search";
 import SearchResultList from "./SearchResultList";
+import { Box } from "@mui/system";
+import { LoadingSpinner } from "../LoadingSpinner";
 
 const EmptyPlaylistWithSearch = () => {
   const [keyword, setKeyword] = useState<string>("");
@@ -14,24 +16,47 @@ const EmptyPlaylistWithSearch = () => {
     setKeyword(e.target.value);
   };
 
-  if (error) {
-  }
+  // vvxsd
+
+  const validEntries =
+    data?.pages.map((item) => Object.entries(item).filter(([_, value]) => value?.items?.length > 0)) ?? [];
+
+  const hasResult = validEntries.some((entries) => entries.length > 0);
 
   return (
-    <div style={{ width: "100%" }}>
+    <Container>
       <Typography variant="h1" my={"10px"}>
         플레이리스트에 추가할 곡을 찾아보세요
       </Typography>
       <TextField value={keyword} onChange={handleSearchKeyword} style={{ width: "100%" }} />
-      {data?.pages.map((item, idx) => {
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : hasResult ? (
+        validEntries.map((entryGroup, pageIdx) =>
+          entryGroup.map(([key, value]) => <SearchResultList key={`${pageIdx}-${key}`} list={value.items} />)
+        )
+      ) : keyword === "" ? (
+        <></>
+      ) : (
+        <div>{`No Result for ${keyword}`}</div>
+      )}
+      {/* {data?.pages.map((item, idx) => {
         const validEntries = Object.entries(item).filter(([_, value]) => value?.items?.length > 0);
         if (validEntries.length === 0) return null;
         return validEntries.map(([key, value]) => {
           return <SearchResultList key={`${idx}-${key}`} list={value.items} />;
         });
-      })}
-    </div>
+      })}*/}
+    </Container>
   );
 };
+
+const Container = styled(Box)({
+  width: "100%",
+  padding: `1rem`,
+  display: "flex",
+  flexDirection: "column",
+  gap: `1rem`,
+});
 
 export default EmptyPlaylistWithSearch;
--- src/components/playlist/SearchResultList.tsx
+++ src/components/playlist/SearchResultList.tsx
@@ -7,26 +7,27 @@ interface SearchResultListProps {
 const SearchResultList = ({ list }: SearchResultListProps) => {
   if (!list || list.length === 0) return null;
 
-  const isTrackList = "album" in list[0] && "artists" in list[0];
+  // const isTrackList = list.length > 0 && "album" in list[0] && "artists" in list[0];
+  const isTrackList = list.length > 0 && list[0] != null && "album" in list[0] && "artists" in list[0];
 
   if (!isTrackList) return null;
 
   return (
     <div>
-      {list.map((track: any) => (
-        <StyledTableRow key={track.id}>
+      {list.map((item: any) => (
+        <StyledTableRow key={item.id}>
           <TableCell>
             <Box display="flex" alignItems="center">
               <Box>
-                <AlbumImage src={track.album?.images?.[0]?.url} width="40px" />
+                <AlbumImage src={item.album?.images?.[0]?.url} width="40px" />
               </Box>
               <Box>
-                <Typography fontWeight={700}>{track.name}</Typography>
-                <Typography color="text.secondary">{track.artists?.[0]?.name ?? "Unknown Artist"}</Typography>
+                <Typography fontWeight={700}>{item.name}</Typography>
+                <Typography color="text.secondary">{item.artists?.[0]?.name ?? "Unknown Artist"}</Typography>
               </Box>
             </Box>
           </TableCell>
-          <TableCell>{track.album?.name}</TableCell>
+          <TableCell>{item.album?.name}</TableCell>
           <TableCell>
             <Button>Add</Button>
           </TableCell>
