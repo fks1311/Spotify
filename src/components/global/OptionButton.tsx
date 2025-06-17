25f217629a6836c892a8cc6bf093fe75a4e8babb
ranyoung <fks1311@gmail.com>
2025-06-17 23:24:02 +0900

    refactor: 재생목록 이름 변경, 삭제 등 기능 생성 및 context api 적용

--- src/components/global/OptionButton.tsx
+++ src/components/global/OptionButton.tsx
@@ -1,20 +1,23 @@
 import { styled } from "@mui/material";
 import { useOpenContext } from "./ContextProvider";
-import { useUnfollowPlaylist } from "../../hooks/useUnfollowPlaylist";
 import { useParams } from "react-router";
 
 const OptionButton = () => {
-  const { option, setOption } = useOpenContext();
-  const { mutate: unfollowPlaylist } = useUnfollowPlaylist();
+  const { option } = useOpenContext();
   const { id } = useParams<{ id: string }>();
-  const { modal, setModal } = useOpenContext();
+  const { setModal } = useOpenContext();
 
-  const handleChangePlaylistName = () => {};
-  const handleChangeCoverImg = () => {};
+  const handleChangePlaylistName = () => {
+    if (id) {
+      setModal({ isOpen: true, type: "change", txt: "해당 재생목록의 이름을 변경하시겠습니까?" });
+    }
+  };
+  const handleChangeCoverImg = () => {
+    alert("진행 예정입니다.");
+  };
   const handleUnfollowPlaylist = () => {
     if (id) {
-      setModal({ isOpen: true, data: undefined, type: "unfollow" });
-      // unfollowPlaylist({ id: id, ids: [id] });
+      setModal({ isOpen: true, type: "unfollow", txt: "해당 재생목록을 삭제하시겠습니까?" });
     }
   };
 
