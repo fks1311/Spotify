b70024cce9323a4c5f9d6b2043ed84dfcc3c8fbf
ranyoung <fks1311@gmail.com>
2025-06-17 10:36:56 +0900

    refactor: 플레이리스트 목록 전부 삭제되었을 때 예외 처리

--- src/hooks/useRemovePlaylistItems.ts
+++ src/hooks/useRemovePlaylistItems.ts
@@ -3,11 +3,14 @@ import { removePlaylistItems } from "../apis/playlistApi";
 import { RemovePlaylistItemsRequest } from "../models/playlist";
 import { useGetCurrentUserProfile } from "./useGetCurrentUserProfile";
 import { useOpenContext } from "../components/global/ContextProvider";
+import { useSetRecoilState } from "recoil";
+import { triggerAtom } from "../utils/atom";
 
 export const useRemovePlaylistItems = () => {
   const queryClient = useQueryClient();
   const { data: user } = useGetCurrentUserProfile();
   const { open, setOpen } = useOpenContext();
+  const setTrigger = useSetRecoilState(triggerAtom);
 
   return useMutation({
     mutationFn: (params: RemovePlaylistItemsRequest) => {
@@ -19,6 +22,7 @@ export const useRemovePlaylistItems = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["playlist-items"] });
       setOpen({ isOpen: false, data: undefined });
+      setTrigger((prev) => prev + 1);
     },
   });
 };
