6f070d72c03ad25c1a6ad5a0213e31d2ad76656a
ranyoung <fks1311@gmail.com>
2025-06-18 23:11:22 +0900

    feat: 홈화면 플레이리스트 컴포넌트 생성

---
+++ src/components/homepage/NewestPlaylist.tsx
@@ -0,0 +1,46 @@
+import { Box, Typography } from "@mui/material";
+import { useSearchCategory } from "../../hooks/useSearchCategory";
+import TrackListLayout from "../../layout/homepage/TrackListLayout";
+import { SEARCH_TYPE } from "../../models/search";
+import { LoadingSpinner } from "../global/LoadingSpinner";
+import { ListContainer } from "./NewReleases";
+import Card from "../album/Card";
+
+const NewestPlaylist = () => {
+  const { data, isLoading } = useSearchCategory({
+    q: "k-pop",
+    type: [SEARCH_TYPE.Playlist],
+  });
+
+  if (isLoading) return <LoadingSpinner />;
+
+  const playlists = data?.pages[0]?.playlists?.items ?? [];
+
+  return (
+    <TrackListLayout title="플레이리스트">
+      {playlists.length > 0 ? (
+        <ListContainer>
+          {playlists.map((artist: any) => {
+            if (artist !== null) {
+              return (
+                <Box
+                  key={artist.id}
+                  sx={{
+                    minWidth: 180,
+                    marginBottom: "10px",
+                  }}
+                >
+                  <Card name={artist.name} image={artist.images[0].url} />
+                </Box>
+              );
+            }
+          })}
+        </ListContainer>
+      ) : (
+        <Typography variant="h2">No data</Typography>
+      )}
+    </TrackListLayout>
+  );
+};
+
+export default NewestPlaylist;
