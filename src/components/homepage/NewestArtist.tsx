7511caa3bec6c42282806fb5a894ca9124fb81c6
ranyoung <fks1311@gmail.com>
2025-06-18 23:01:33 +0900

    feat: 홈화면 아티스트 컴포넌트 생성

---
+++ src/components/homepage/NewestArtist.tsx
@@ -0,0 +1,44 @@
+import { Box, Typography } from "@mui/material";
+import { useSearchCategory } from "../../hooks/useSearchCategory";
+import TrackListLayout from "../../layout/homepage/TrackListLayout";
+import { SEARCH_TYPE } from "../../models/search";
+import { LoadingSpinner } from "../global/LoadingSpinner";
+import { ListContainer } from "./NewReleases";
+import Card from "../album/Card";
+
+const NewestArtist = () => {
+  const { data, isLoading } = useSearchCategory({
+    q: "k-pop",
+    type: [SEARCH_TYPE.Artist],
+    limit: 10,
+  });
+
+  if (isLoading) return <LoadingSpinner />;
+
+  const artists = data?.pages[0]?.artists?.items ?? [];
+  console.log(artists);
+
+  return (
+    <TrackListLayout title="아티스트">
+      {artists.length > 0 ? (
+        <ListContainer>
+          {artists.map((artist: any) => (
+            <Box
+              key={artist.id}
+              sx={{
+                minWidth: 180,
+                marginBottom: "10px",
+              }}
+            >
+              <Card name={artist.name} image={artist.images[0].url} />
+            </Box>
+          ))}
+        </ListContainer>
+      ) : (
+        <Typography variant="h2">No data</Typography>
+      )}
+    </TrackListLayout>
+  );
+};
+
+export default NewestArtist;
