dcea71ad23f2b31122f89fefc48df281e8591aab
ranyoung <fks1311@gmail.com>
2025-06-18 23:22:22 +0900

    feat: 홈화면 앨범 추가

---
+++ src/components/homepage/NewestAlbum.tsx
@@ -0,0 +1,43 @@
+import { Box, Typography } from "@mui/material";
+import { useSearchCategory } from "../../hooks/useSearchCategory";
+import TrackListLayout from "../../layout/homepage/TrackListLayout";
+import { SEARCH_TYPE } from "../../models/search";
+import { LoadingSpinner } from "../global/LoadingSpinner";
+import { ListContainer } from "./NewReleases";
+import Card from "../album/Card";
+
+const NewestAlbum = () => {
+  const { data, isLoading } = useSearchCategory({
+    q: "k-pop",
+    type: [SEARCH_TYPE.Album],
+    limit: 10,
+  });
+
+  if (isLoading) return <LoadingSpinner />;
+
+  const album = data?.pages[0]?.albums?.items ?? [];
+
+  return (
+    <TrackListLayout title="앨범">
+      {album.length > 0 ? (
+        <ListContainer>
+          {album.map((artist: any) => (
+            <Box
+              key={artist.id}
+              sx={{
+                minWidth: 180,
+                marginBottom: "10px",
+              }}
+            >
+              <Card name={artist.name} image={artist.images[0].url} />
+            </Box>
+          ))}
+        </ListContainer>
+      ) : (
+        <Typography variant="h2">No data</Typography>
+      )}
+    </TrackListLayout>
+  );
+};
+
+export default NewestAlbum;
