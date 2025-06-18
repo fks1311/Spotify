00ff23eff0f9646ec984f6f2875fdfb2a16dbe05
ranyoung <fks1311@gmail.com>
2025-06-18 18:58:55 +0900

    feat: 카테고리 홈 생성

---
+++ src/components/homepage/ServeralCategories.tsx
@@ -0,0 +1,31 @@
+import { Box } from "@mui/system";
+import { useGetServeralCategories } from "../../hooks/useGetServeralCategories";
+import TrackListLayout from "../../layout/homepage/TrackListLayout";
+import { LoadingSpinner } from "../global/LoadingSpinner";
+import { ListContainer } from "./NewReleases";
+import { Typography } from "@mui/material";
+import Card from "../album/Card";
+
+const ServeralCategories = () => {
+  const { data, isLoading } = useGetServeralCategories({ limit: 10, offset: 0 });
+
+  if (isLoading) return <LoadingSpinner />;
+
+  return (
+    <TrackListLayout title="카테고리">
+      {data && data?.pages[0]?.categories?.items.length > 0 ? (
+        <ListContainer>
+          {data?.pages[0]?.categories?.items.map((category: any, idx: number) => (
+            <Box key={category.id}>
+              <Card name={category.name} image={category.icons[0].url} />{" "}
+            </Box>
+          ))}
+        </ListContainer>
+      ) : (
+        <Typography variant="h2">No data</Typography>
+      )}
+    </TrackListLayout>
+  );
+};
+
+export default ServeralCategories;
