17dc2e1da92c059d2eec07876f29af818bafe38c
ranyoung <fks1311@gmail.com>
2025-06-18 12:09:15 +0900

    feat: refreshToken 훅 생성

--- src/hooks/useExchangeToken.ts
+++ src/hooks/useExchangeToken.ts
@@ -1,5 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
-import { exchageToken } from "../apis/authApi";
+import { exchageToken, getRefreshToken } from "../apis/authApi";
 import { ExchageTokenResponse } from "../models/auth";
 
 /** 사용자가 권한 부여 요청을 수락하면 권한 부여 코드를 액세스 토큰으로 교환하는 커스텀 훅입니다. */
@@ -10,6 +10,22 @@ export const useExchangeToken = () => {
     },
     onSuccess: (data) => {
       localStorage.setItem("access_token", data.access_token);
+      localStorage.setItem("refresh_token", data.refresh_token);
+      localStorage.setItem("expires_at", (Date.now() + data.expires_in * 1000).toString());
+    },
+  });
+};
+
+export const useExchangeRefreshToken = () => {
+  return useMutation<ExchageTokenResponse, Error, { refreshToken: string }>({
+    mutationFn: ({ refreshToken }) => {
+      return getRefreshToken(refreshToken);
+    },
+    onSuccess: (data) => {
+      localStorage.setItem("access_token", data.access_token);
+      if (data.refresh_token) {
+        localStorage.setItem("refresh_token", data.refresh_token);
+      }
     },
   });
 };
