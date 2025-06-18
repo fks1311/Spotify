a1455ef2b55487bab803e8285d0251df1fe906ed
ranyoung <fks1311@gmail.com>
2025-06-18 14:55:21 +0900

    refactor: limit 변경

--- src/apis/albumApi.ts
+++ src/apis/albumApi.ts
@@ -5,7 +5,7 @@ import { IGetNewReleasesResponse } from "../models/album";
 /** Spotify API : 앨범 발매 목록을 가져옵니다. */
 export const getNewReleases = async (clientCredentialToken: string): Promise<IGetNewReleasesResponse> => {
   try {
-    const response = await axios.get(`${SPOTIFY_BASE_URL}/browse/new-releases?limit=6`, {
+    const response = await axios.get(`${SPOTIFY_BASE_URL}/browse/new-releases?limit=10`, {
       headers: {
         Authorization: `Bearer ${clientCredentialToken}`,
       },
