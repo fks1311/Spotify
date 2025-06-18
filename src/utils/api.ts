2c9757e2a06d5852c1d1e3617fb4d56e9fe9da30
ranyoung <fks1311@gmail.com>
2025-06-18 12:10:24 +0900

    refactor: 액세스 토큰 만료 및 리프레시 로직 추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,19 +1,19 @@
 import axios from "axios";
 import { SPOTIFY_BASE_URL } from "../configs/commonConfig";
+import { getValidAccessToken } from "./getValidAccessToken";
 
 export const api = axios.create({
   baseURL: `${SPOTIFY_BASE_URL}`,
   timeout: 1000,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${localStorage.getItem("access_token")}`,
   },
 });
 
 // 요청 인터셉터 추가하기
-api.interceptors.request.use((request) => {
-  // 요청이 전달되기 전에 작업 수행
-  request.headers.Authorization = `Bearer ${localStorage.getItem("access_token")}`;
+api.interceptors.request.use(async (request) => {
+  const token = await getValidAccessToken();
+  request.headers.Authorization = `Bearer ${token}`;
 
   return request;
 });
