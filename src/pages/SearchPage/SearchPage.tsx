6f1e39428f582b677e10c502a65474ce317e60ff
ranyoung <fks1311@gmail.com>
2025-06-21 15:52:50 +0900

    feat: SearchPage 생성

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { Box, Grid, styled, Typography } from "@mui/material";
+import { LoadingSpinner } from "../../components/global/LoadingSpinner";
+import { useGetServeralCategories } from "../../hooks/useGetServeralCategories";
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const { data, isLoading } = useGetServeralCategories({ limit: 20, offset: 0 });
+
+  if (isLoading) return <LoadingSpinner />;
+
+  return (
+    <Container className="container">
+      {data && data?.pages[0]?.categories?.items.length > 0 ? (
+        <Grid container spacing={2}>
+          {data?.pages[0]?.categories?.items.map((category: any, idx: number) => (
+            <Content key={category.id} size={{ xs: 12, sm: 6, md: 4 }}>
+              <Contentbox>
+                <CategoryName variant="h1">{category.name}</CategoryName>
+                <CategoryImg src={category.icons[0].url} />
+              </Contentbox>
+            </Content>
+          ))}
+        </Grid>
+      ) : (
+        <Typography variant="h2">No data</Typography>
+      )}
+    </Container>
+  );
 };
+
+const Container = styled("div")({
+  marginTop: "1rem",
+});
+const Content = styled(Grid)({});
+const Contentbox = styled(Box)({
+  position: "relative",
+  backgroundColor: "#C83F12",
+  borderRadius: "10px",
+  overflow: "hidden",
+  aspectRatio: "2 / 1",
+});
+const CategoryName = styled(Typography)({
+  padding: "1rem",
+});
+const CategoryImg = styled("img")({
+  position: "absolute",
+  width: "45%",
+  bottom: "-10%",
+  right: "-8%",
+  transform: `rotate(25deg)`,
+});
 
 export default SearchPage;
