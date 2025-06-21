26a16bdda30ed9263634788a1c9f3a9b6a140bd7
ranyoung <fks1311@gmail.com>
2025-06-21 15:45:23 +0900

    feat: SearchBar 컴포넌트 생성

---
+++ src/components/common/SearchBar.tsx
@@ -0,0 +1,39 @@
+import { InputAdornment, styled, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+interface SearchBarProps {
+  keyword: string;
+  setKeyword: React.Dispatch<React.SetStateAction<string>>;
+  placeholder: string;
+}
+const SearchBar = ({ keyword, setKeyword, placeholder }: SearchBarProps) => {
+  const handleSearchKeyword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+  };
+
+  return (
+    <Container
+      value={keyword}
+      onChange={handleSearchKeyword}
+      placeholder={placeholder}
+      slotProps={{
+        input: {
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon style={{ color: "white" }} />
+            </InputAdornment>
+          ),
+        },
+      }}
+    />
+  );
+};
+
+const Container = styled(TextField)({
+  width: "40%",
+  "& .MuiInputBase-root ": {
+    borderRadius: "30px",
+  },
+});
+
+export default SearchBar;
