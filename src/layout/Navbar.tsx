0d6a451ec5edb4e79a3c4faa53e690e8d1d76624
ranyoung <fks1311@gmail.com>
2025-06-21 15:42:52 +0900

    refactor: NavBar에 SearchBar 추가

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, styled } from "@mui/material";
+import { Box, InputAdornment, styled, TextField } from "@mui/material";
 import LoginButton from "../components/global/LoginButton";
 import { useGetCurrentUserProfile } from "../hooks/useGetCurrentUserProfile";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
@@ -6,6 +6,9 @@ import { getLocalStorageSafe } from "../utils/localStorage";
 import { useSetRecoilState } from "recoil";
 import { triggerAtom } from "../utils/atom";
 import { useQueryClient } from "@tanstack/react-query";
+import { useLocation } from "react-router";
+import SearchBar from "../components/common/SearchBar";
+import { useState } from "react";
 
 // trigger
 const Navbar = () => {
@@ -14,6 +17,8 @@ const Navbar = () => {
   const accessToken = getLocalStorageSafe("access_token");
   const url = new URL(window.location.href);
   const queryClient = useQueryClient();
+  const { pathname } = useLocation();
+  const [keyword, setKeyword] = useState<string>("");
 
   const logout = () => {
     window.localStorage.clear();
@@ -39,12 +44,24 @@ const Navbar = () => {
   };
 
   return (
-    <Box display={"flex"} justifyContent={"flex-end"} alignItems={"center"} height={"64px"}>
+    <Container pathname={pathname}>
+      {pathname === "/search" && (
+        <SearchBar keyword={keyword} setKeyword={setKeyword} placeholder="어떤 컨텐츠를 감상하고 싶으세요?" />
+      )}
       {!accessToken || !userProfile ? <LoginButton /> : <IsProfileImg />}
-    </Box>
+    </Container>
   );
 };
 
+interface ContainerStyleProps {
+  pathname: string;
+}
+const Container = styled(Box)<ContainerStyleProps>(({ pathname }) => ({
+  display: "flex",
+  justifyContent: pathname === "/search" ? "space-between" : "flex-end",
+  alignItems: "center",
+  height: "64px",
+}));
 const ProfileImg = styled("div")({
   display: "flex",
   flexDirection: "column",
