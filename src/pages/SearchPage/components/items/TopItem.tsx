c7e518e70effd792aecf8ee6c639f85aec761176
ranyoung <fks1311@gmail.com>
2025-06-22 00:01:08 +0900

    feat: 검색결과 페이지 > 곡 컴포넌트 생성

---
+++ src/pages/SearchPage/components/items/TopItem.tsx
@@ -0,0 +1,11 @@
+import ItemLayout from "../../../../layout/searchpage/ItemLayout";
+
+const TopItem = () => {
+  return (
+    <ItemLayout title="상위 결과" width="30%">
+      <div></div>
+    </ItemLayout>
+  );
+};
+
+export default TopItem;
---
+++ src/pages/SearchPage/components/items/TrackItem.tsx
@@ -0,0 +1,41 @@
+import { Avatar, Box, styled, Typography } from "@mui/material";
+import ItemLayout from "../../../../layout/searchpage/ItemLayout";
+import { ITrack } from "../../../../models/track";
+import moment from "moment";
+
+interface TrackItemProps {
+  tracks: ITrack[];
+}
+const TrackItem = ({ tracks }: TrackItemProps) => {
+  return (
+    <ItemLayout title="곡" width="70%">
+      <TrackLayout>
+        {tracks?.map((track) => (
+          <Track key={track.id}>
+            <Left className="track-in-box">
+              <Avatar variant="square" src={track.album?.images[0].url} sx={{ borderRadius: "10px" }} />
+              <div>
+                <Typography variant="body1">{track.name}</Typography>
+                <Typography variant="body2">{track?.artists?.[0]?.name ?? "No name"}</Typography>
+              </div>
+            </Left>
+            <div>{moment(track.duration_ms).format("mm:ss")}</div>
+          </Track>
+        ))}
+      </TrackLayout>
+    </ItemLayout>
+  );
+};
+
+const TrackLayout = styled(Box)({ display: "flex", flexDirection: "column", gap: "1rem" });
+const Track = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+const Left = styled("div")({
+  display: "flex",
+  gap: "1rem",
+});
+
+export default TrackItem;
