066730ed1561bcb51b29776abd47db9d1b9a9f97
ranyoung <fks1311@gmail.com>
2025-06-21 18:56:10 +0900

    refactor: trigger 전역상태관리 변수 제거 & SearchWithPage 라우터 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React, { Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router";
 import { useExchangeToken } from "./hooks/useExchangeToken";
-import { useRecoilValue } from "recoil";
-import { triggerAtom } from "./utils/atom";
 const AppLayout = React.lazy(() => import("./layout/AppLayout"));
 const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
 const SearchPage = React.lazy(() => import("./pages/SearchPage/SearchPage"));
-const SearchWithPage = React.lazy(() => import("./pages/SearchPage/SearchWithPage"));
 const PlaylistDetailPage = React.lazy(() => import("./pages/PlaylistPage/PlaylistDetailPage"));
 const PlaylistPage = React.lazy(() => import("./pages/PlaylistPage/PlaylistPage"));
 
 function App() {
-  const trigger = useRecoilValue(triggerAtom);
   const urlParams = new URLSearchParams(window.location.search);
   let code = urlParams.get("code");
   const codeVerifier = localStorage.getItem("code_verifier");
@@ -33,7 +29,6 @@ function App() {
         <Route path="/" element={<AppLayout />}>
           <Route index element={<HomePage />} />
           <Route path="search" element={<SearchPage />} />
-          <Route path="search/:keyword" element={<SearchWithPage />} />
           <Route path="playlist/:id" element={<PlaylistDetailPage />} />
           <Route path="/playlist" element={<PlaylistPage />} />
         </Route>
