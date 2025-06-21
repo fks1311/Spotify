5bde19f28ae6d477ffff9e0eecbb543bad3a5094
ranyoung <fks1311@gmail.com>
2025-06-21 18:57:07 +0900

    feat: SearchPage 컴포넌트 생성

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,52 +1,16 @@
-import { Box, Grid, styled, Typography } from "@mui/material";
-import { LoadingSpinner } from "../../common/components/LoadingSpinner";
-import { useGetServeralCategories } from "../../hooks/useGetServeralCategories";
+import { useState } from "react";
+import SearchLayout from "../../layout/searchpage/SearchLayout";
+import DefaultSearchPage from "./components/DefaultSearchPage";
+import SearchWithPage from "./components/SearchWithPage";
 
 const SearchPage = () => {
-  const { data, isLoading } = useGetServeralCategories({ limit: 20, offset: 0 });
-
-  if (isLoading) return <LoadingSpinner />;
+  const [keyword, setKeyword] = useState<string>("");
 
   return (
-    <Container className="container">
-      {data && data?.pages[0]?.categories?.items.length > 0 ? (
-        <Grid container spacing={2}>
-          {data?.pages[0]?.categories?.items.map((category: any, idx: number) => (
-            <Content key={category.id} size={{ xs: 12, sm: 6, md: 4 }}>
-              <Contentbox>
-                <CategoryName variant="h1">{category.name}</CategoryName>
-                <CategoryImg src={category.icons[0].url} />
-              </Contentbox>
-            </Content>
-          ))}
-        </Grid>
-      ) : (
-        <Typography variant="h2">No data</Typography>
-      )}
-    </Container>
+    <SearchLayout keyword={keyword} setKeyword={setKeyword}>
+      {keyword === "" ? <DefaultSearchPage /> : <SearchWithPage />}
+    </SearchLayout>
   );
 };
-
-const Container = styled("div")({
-  marginTop: "1rem",
-});
-const Content = styled(Grid)({});
-const Contentbox = styled(Box)({
-  position: "relative",
-  backgroundColor: "#C83F12",
-  borderRadius: "10px",
-  overflow: "hidden",
-  aspectRatio: "2 / 1",
-});
-const CategoryName = styled(Typography)({
-  padding: "1rem",
-});
-const CategoryImg = styled("img")({
-  position: "absolute",
-  width: "45%",
-  bottom: "-10%",
-  right: "-8%",
-  transform: `rotate(25deg)`,
-});
 
 export default SearchPage;
