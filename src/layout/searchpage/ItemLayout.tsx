c7670e8ef1ca5c228450f8706346ea84c7584aa9
ranyoung <fks1311@gmail.com>
2025-06-22 16:08:00 +0900

    refactor: SearchItemLayout 타입 수정

--- src/layout/searchpage/ItemLayout.tsx
+++ src/layout/searchpage/ItemLayout.tsx
@@ -2,19 +2,19 @@ import { Box, styled, Typography } from "@mui/material";
 
 interface ItemLayoutProps {
   title: string;
-  width: string;
+  width: string | { [key: string]: string };
   children: React.ReactNode;
 }
 const ItemLayout = ({ title, width, children }: ItemLayoutProps) => {
   return (
-    <Layout width={width}>
+    <Layout sx={{ width }}>
       <Title variant="h5">{title}</Title>
       {children}
     </Layout>
   );
 };
 
-const Layout = styled(Box)(({ width }) => ({ padding: "1rem", width: `${width}` }));
+const Layout = styled(Box)(({ width }) => ({ padding: "1rem" }));
 const Title = styled(Typography)({ fontWeight: 700, marginBottom: "1rem" });
 
 export default ItemLayout;
