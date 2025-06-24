570110b6cabdf7d34a260000598ecc3607959c09
ranyoung <fks1311@gmail.com>
2025-06-24 14:32:35 +0900

    feat: toast 라이브러리 추가 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router";
 import { useExchangeToken } from "./hooks/useExchangeToken";
 import { useRecoilValue } from "recoil";
 import { triggerAtom } from "./utils/atom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const AppLayout = React.lazy(() => import("./layout/AppLayout"));
 const HomePage = React.lazy(() => import("./pages/HomePage/HomePage"));
 const SearchPage = React.lazy(() => import("./pages/SearchPage/SearchPage"));
@@ -27,16 +29,19 @@ function App() {
   }, [code, codeVerifier, trigger]);
 
   return (
-    <Suspense fallback={<div>loading...</div>}>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="search" element={<SearchPage />} />
-          <Route path="playlist/:id" element={<PlaylistDetailPage />} />
-          <Route path="/playlist" element={<PlaylistPage />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="search" element={<SearchPage />} />
+            <Route path="playlist/:id" element={<PlaylistDetailPage />} />
+            <Route path="/playlist" element={<PlaylistPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
